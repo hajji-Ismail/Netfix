a5dbdb16c01cfb0ca174bdb491ab0639b31ce7bf
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-08-16 12:09:25 +0100

    delete 6001 files

--- netfixproject/Auth/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from .models import Register
-class register(forms.ModelForm):
-    class meta :
-        model = Register
-        fields ='__all__'
--- netfixproject/Auth/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from Auth import models
-
-def register(request) :
- return render(request , 'Auth/register.html')
